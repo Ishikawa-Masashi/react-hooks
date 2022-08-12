762bc3b67a34c8633fc3b4ac09a4002d924afed0
IshikawaMasashi <IshikawaMasashi@outlook.com>
2022-08-13 06:47:14 +0900

    fix

--- src/useAnimationFrame.ts
+++ src/useAnimationFrame.ts
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 
 export function useAnimationFrame(callback: () => void) {
   const requestRef = React.useRef<ReturnType<typeof requestAnimationFrame>>();
